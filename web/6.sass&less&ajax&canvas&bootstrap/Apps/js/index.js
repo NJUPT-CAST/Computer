40ebf32c3e9a4296d59e66938a9ec091b961f332
glassysky <873661157@qq.com>
2016-03-09 22:36:43 +0800

    new lesson

--- web/6.sass&less&ajax&canvas&bootstrap/Apps/js/index.js
+++ web/6.sass&less&ajax&canvas&bootstrap/Apps/js/index.js
@@ -2,11 +2,26 @@
  * Created by cui on 2015/12/6.
  */
 $(document).ready(function(){
+    var str = "";
+
+    str = $("h1").html();
+    console.log(str);
+
     $.ajax({
         url : "res/data.json",
         type : "get",
         success : function(callback){
-            console.log(callback);
+            if(callback.status == "success"){
+                var name = callback.msg.name;
+                str = str + name;
+                $("h1").html(str);
+            } else {
+                str = str + "世界";
+                $("h1").html(str);
+            }
+        },
+        error : function(){
+
         }
     });
 });
